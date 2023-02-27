09dc76e3c83e8b0025b5bab3e7d481fc1c122a56
Fabian Castaño <fabioc9675@gmail.com>
2023-02-27 18:46:51 -0500

    modification in python script

--- Xilinx_Prjs/Kria_KR260/python_test/gpio_test.py
+++ Xilinx_Prjs/Kria_KR260/python_test/gpio_test.py
@@ -5,19 +5,19 @@ import subprocess
 import multiprocessing
 
 for i in range(0,10):
-    os.system('echo 1 > /sys/class/gpio/gpio480/value') # Turn on the led
+    os.system('echo 1 > /sys/class/gpio/gpio492/value') # Turn on the led
     time.sleep(1)
-    os.system('echo 0 > /sys/class/gpio/gpio480/value') # Turn off the led
+    os.system('echo 0 > /sys/class/gpio/gpio492/value') # Turn off the led
     time.sleep(1)
 
     # also we can to read the state of a pin in case it is an input
-    gpio_in_pin = open('/sys/class/gpio/gpio320/value', 'r')
+    gpio_in_pin = open('/sys/class/gpio/gpio493/value', 'r')
     gpio_in = gpio_in_pin.read()
     print('entrada = ', gpio_in)
 
-    if gpio_in == 1:
-        os.system('echo 1 > /sys/class/gpio/gpio481/value')
+    if int(gpio_in) == 1:
+        os.system('echo 1 > /sys/class/gpio/gpio494/value')
     else:
-        os.system('echo 0 > /sys/class/gpio/gpio481/value')
+        os.system('echo 0 > /sys/class/gpio/gpio494/value')
 
 
